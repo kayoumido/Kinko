f7fb009abf278873ba0267fe08e624aa1d93c2d9
Doran Kayoumi <dorankayoumi@gmail.com>
2021-05-24 22:43:01 +0200

    Add new seed

--- migrations/2021-05-18-080745_create_files/up.sql
+++ migrations/2021-05-18-080745_create_files/up.sql
@@ -2,7 +2,8 @@ create table files (
     id serial primary key,
     name varchar not null,
     symmetric_key varchar not null,
-    nonce varchar not null,
+    content_nonce varchar not null,
+    name_nonce varchar not null,
     owner_id integer not null,
 
     constraint fk_ownership
--- migrations/2021-05-18-131457_seed/up.sql
+++ migrations/2021-05-18-131457_seed/up.sql
@@ -1,1 +1,14 @@
-insert into users (username, public_key, shared_secret, shared_secret_salt) values ('AliceXOXO', 'BJnRg+bze7YdwWKfKODuwewCqdvCLxEiMbgjGORbwMK1xqRBcWpr8dILxJFlfaqefTBzInRzHtdysSHGAxoj7M8=', 'CpP6H2YRfRSStCBwmmB7eAI00rlZCeOKmNXlZ7DyIVY=', 'esM6awwBCndMbzph6SvwXggvsE0B38742FU8lokT2P8=');
+insert into users (username, public_key, shared_secret, shared_secret_salt) values (
+    'AliceXOXO', 
+    'BJnRg+bze7YdwWKfKODuwewCqdvCLxEiMbgjGORbwMK1xqRBcWpr8dILxJFlfaqefTBzInRzHtdysSHGAxoj7M8=', 
+    'CpP6H2YRfRSStCBwmmB7eAI00rlZCeOKmNXlZ7DyIVY=', 
+    'esM6awwBCndMbzph6SvwXggvsE0B38742FU8lokT2P8='
+);
+
+insert into files (name, symmetric_key, content_nonce, name_nonce, owner_id) values (
+    'Fo_ZakTJWBWP9GUgVnoVaqWOU4EIdDUoAThk_R8=',
+    'BJGNBiT6G4CdAkH4qdKccnb3F9XsVpf3sTvkleG8hf+6W7Q9e1Zf+SsTrjNqsi25RekJnbzZmBGtiQjW/We9DWLrV5givGjqQLaT2KLoFOXthrZsNSSLJeOdEqBi5NcV4mafKPjewa4UGVCFsFwBHxvYFeleVXnKH7O+Lja7lTs2',
+    'yiwNSsCiJSBgnSFh',
+    'TMaD2teGSROQpFeW',
+    1
+);
